ef74ecdd9b77bbfeac46354e20894f31bb29ebe3
Paula P <patrunjel.paula@gmail.com>
2019-06-05 00:40:48 +0200

    slider error fixed

--- js/scripts.js
+++ js/scripts.js
@@ -44,20 +44,21 @@ $(() => {
         $subscribeModal.modal('show');
     });
 
+    /* Slider from Subjects & Teaching page */
 	$("#dropdown-boxes-btn-one").click(function(){
-		$("#dropdown-boxes-one").slideToggle("fast");
+		$("#dropdown-boxes-one").toggle();
 		$("#dropdown-arrow-right-one").toggle();
 		$("#dropdown-arrow-down-one").toggle();
 	});
 	
 	$("#dropdown-boxes-btn-two").click(function(){
-		$("#dropdown-boxes-two").slideToggle("fast"),
+		$("#dropdown-boxes-two").toggle();
 		$("#dropdown-arrow-right-two").toggle();
 		$("#dropdown-arrow-down-two").toggle();
 	});
 	
 	$("#dropdown-boxes-btn-three").click(function(){
-		$("#dropdown-boxes-three").slideToggle("fast");
+		$("#dropdown-boxes-three").toggle();
 		$("#dropdown-arrow-right-three").toggle();
 		$("#dropdown-arrow-down-three").toggle();
 	});
